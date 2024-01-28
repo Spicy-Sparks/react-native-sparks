3e1bc9ba51021905e7ef86f8ffb4677ed7713af2
Marco Fontana <fontana.marco@hotmail.com>
2024-01-28 12:01:50 +0100

    Update SparksPackage.m

--- ios/Sparks/SparksPackage.m
+++ ios/Sparks/SparksPackage.m
@@ -1,5 +1,9 @@
 #import "Sparks.h"
+#if __has_include(<SSZipArchive/SSZipArchive.h>)
+#import <SSZipArchive/SSZipArchive.h>
+#else
 #import "SSZipArchive.h"
+#endif
 
 @implementation SparksPackage
 
